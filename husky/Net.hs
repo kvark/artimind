ec0e992a500cc71a937ce92899b8c8e552c297db
kvark <kvark@localhost>
2012-01-16 13:48:23 -0500

    hgignore improved

---
+++ husky/Net.hs
@@ -0,0 +1,26 @@
+module AI.Net
+( Mind
+) where
+
+import AI.Core
+
+
+data Neuron = Neuron	{
+}deriving (Show,Eq)
+
+
+data Link =	Link	{
+	source	:: Neuron,
+	target	:: Neuron
+}deriving (Show,Eq)
+
+
+data Mind = Mind	{
+	nodes	:: [Neuron],
+	links	:: [Link]
+}deriving ()
+
+
+instance Think Mind where
+	decide _ _ = id
+	learn t _ = t
