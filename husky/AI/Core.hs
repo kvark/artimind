94cf51b33dddc545bcffe60dbb0df0521d4074f4
kvark <kvark@localhost>
2012-01-17 14:25:12 -0500

    Refactored to not use any extensions

--- husky/AI/Core.hs
+++ husky/AI/Core.hs
@@ -1,27 +1,31 @@
 module AI.Core
-(	Body, World, Sensor, Actor,
+(	Body, World,
+	Strength, Sensor, Actor,
 	Think, decide, learn,
-	constFeel, idle, ZeroMind(ZeroMind)
+	constFeel, idleActor, ZeroMind(ZeroMind)
 ) where
 
 
 class Body b
 class World w
 
-type Sensor	= (World w, Body b) => (w,b) -> Int
-type Actor	= (World w, Body b) => (w,b) -> (w,b,Int)
+type Strength = Int
+type Actor w b = (w,b) -> (w,b,Strength)
+type Sensor w b = (w,b) -> Strength
+
 
 class Think t where
-	decide	:: t -> Sensor -> (Actor,String)
-	learn	:: t -> (Actor,Int) -> t
+	decide	:: (World w, Body b) => t -> [Sensor w b] -> (Actor w b,String)
+	learn	:: (World w, Body b) => t -> (Actor w b,Strength) -> t
 
 
+constFeel	:: (World w, Body b) => Strength -> (w,b) -> Strength
 constFeel val _ = val
 
-idle :: (World w, Body b) => (w,b) -> (w,b,Int)
-idle (w,b) = (w,b,0)
+idleActor	:: (World w, Body b) => Actor w b
+idleActor (w,b) = (w,b,0)
 
 data ZeroMind = ZeroMind	{}
 instance Think ZeroMind where
-	decide ZeroMind _ = (idle,"none")
+	decide ZeroMind _ = (idleActor,"none")
 	learn ZeroMind _ = ZeroMind
--- husky/Main.hs
+++ husky/Main.hs
@@ -5,22 +5,22 @@ module Main
 import qualified AI.Core as Ai
 import qualified Env
 
-iterFun	:: (Ai.World w, Ai.Body b, Ai.Think t) => (Ai.Sensor,(w,b,t)) -> ((String,Int),(w,b,t))
+iterFun	:: (Ai.World w, Ai.Body b, Ai.Think t) => ([Ai.Sensor w b],(w,b,t)) -> ((String,Ai.Strength),(w,b,t))
 iterFun (s,(w,b,t)) =
 	let	(actor,description) = Ai.decide t s
 		(w',b',result) = actor (w,b)
 		t' = Ai.learn t (actor,result)
 	in	((description,result), (w',b',t'))
 
-sensor = Ai.constFeel 0
-state :: Int -> ((String,Int), (Env.World,Env.Body,Ai.ZeroMind))
+sensors = [Ai.constFeel 0]
+state :: Int -> (( String, Ai.Strength ),( Env.World, Env.Body, Ai.ZeroMind ))
 state 0 = (("init",0), (Env.World,Env.Body,Ai.ZeroMind))
-state n = iterFun (sensor, snd $ state (n-1))
+state n = iterFun (sensors, snd $ state (n-1))
 
 message 0 = ("", snd $ state 0)
 message n =
 	let	(s,prev) = message (n-1)
-		(info,cur) = iterFun (sensor,prev)
+		(info,cur) = iterFun (sensors,prev)
 		s1 = "\nStep " ++ show n
 		s2 = "\tAction " ++ show (fst info)
 		s3 = "\tResult " ++ show (snd info)
