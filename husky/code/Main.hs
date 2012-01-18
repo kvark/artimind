2512f824002430bd6ef71df86d987b8a1175add8
kvark <kvark@localhost>
2012-01-18 09:29:50 -0500

    File structure changed to include haskell version as a first class citizen. Print output improved.

--- husky/code/Main.hs
+++ husky/code/Main.hs
@@ -2,8 +2,10 @@ module Main
 ( main
 ) where
 
+import Data.List
 import qualified AI.Core as Ai
 import qualified Env
+
 
 iterFun	:: (Ai.World w, Ai.Body b, Ai.Think w b t) => ([Ai.Sensor w b],(w,b,t)) -> ((String,Ai.Energy),(w,b,t))
 iterFun (s,(w,b,t)) =
@@ -12,20 +14,25 @@ iterFun (s,(w,b,t)) =
 		t' = Ai.learn t (actor,result)
 	in	((description,result), (w',b',t'))
 
+
+type State = ( Env.World, Env.Body, Env.TestMind )
 sensors = [Ai.feelConst 0]
-state :: Int -> (( String, Ai.Energy ),( Env.World, Env.Body, Env.TestMind ))
+state	:: Int -> (( String, Ai.Energy ), State)
 state 0 = (( "init", 0 ), ( Env.World 0, Env.Body, Env.TestMind ))
 state n = iterFun (sensors, snd $ state (n-1))
 
-message 0 = ("", snd $ state 0)
+
+message	:: Int -> ([String],State)
+message 0 = ([], snd $ state 0)
 message n =
 	let	(s,prev) = message (n-1)
 		(info,cur) = iterFun (sensors,prev)
-		s1 = "\nStep " ++ show n
+		s1 = "Step " ++ show n
 		s2 = "\tAction " ++ show (fst info)
 		s3 = "\tResult " ++ show (snd info)
-	in	(s ++ s1 ++ s2 ++ s3, cur)
+	in	((s1 ++ s2 ++ s3):s, cur)
+
 
 main = do
-	putStrLn $ fst $ message 10
+	putStrLn $ intercalate "\n" $ reverse $ fst $ message 10
 
